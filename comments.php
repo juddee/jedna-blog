12418b6cca2a1905daed2a4cff81a09a65087a79
juddee <osejuddee@gmail.com>
2024-03-26 12:25:21 +0100

    Fix: Add correct collapse datasets and class to toggle comments if available

--- comments.php
+++ comments.php
@@ -24,23 +24,35 @@ if ( post_password_required() ) {
 
 	<?php
 	// You can start editing here -- including this comment!
+	
 		?>
-		<h2 class="comments-title d-flex justify-content-between">
+		<h2 class="comments-title d-flex justify-content-between" >
 			<?php
 				$jedna_blog_comment_count = get_comments_number();			
 			?>
-			<div>
+			<div 
+				class="comment-count-icon"
+				data-bs-toggle="collapse" 
+				data-bs-target="#commentList" 
+				aria-controls="commentList" 
+				aria-expanded="false" 
+				aria-label="Toggle comments" >
 				<i class="fa-regular fa-comment"></i>
 				<span class="px-1"><?php echo $jedna_blog_comment_count; ?></span>
 			</div>
 
-			<a href="#respond" class="btn btn-outline-secondary">Leave a Comment</a>
+			<a href="#respond" class="btn btn-outline-secondary"
+				data-bs-toggle="collapse" 
+				data-bs-target="#commentForm" 
+				aria-controls="commentForm" 
+				aria-expanded="false" 
+				aria-label="Toggle comment form">Leave a Comment</a>
 
 		</h2><!-- .comments-title -->
 	<?php if ( have_comments() ) : ?>
 		<?php the_comments_navigation(); ?>
 
-		<ol class="comment-list">
+		<ol class="comment-list collapse" id="commentList">
 			<?php
 			wp_list_comments(
 				array(
@@ -62,6 +74,7 @@ if ( post_password_required() ) {
 		endif;
 
 	endif; // Check for have_comments().
+
 
 	comment_form();
 	?>
